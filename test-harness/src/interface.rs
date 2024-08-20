7c53b7fe0124662b747ef6730d9aafb5ef9550c8
Sarmat Khachirov <58251349+sarmatdev@users.noreply.github.com>
2024-08-21 02:24:37 +0300

    Tests for adding liquidity (#2)
    * feat: test setup & `add liquidity` tests
    
    * resolve comments
    
    * wip
    
    * resolve comments
    
    * Cargo fmt
    
    * Improve tests
    
    ---------
    
    Co-authored-by: mpoplavkov <mihail.poplavkov@gmail.com>

---
+++ test-harness/src/interface.rs
@@ -0,0 +1,164 @@
+use fuels::{
+    prelude::*,
+    programs::call_response::FuelCallResponse,
+    types::{input::Input, output::Output, Bits256},
+};
+
+use crate::paths::MOCK_TOKEN_CONTRACT_BINARY_PATH;
+
+use crate::types::PoolId;
+
+abigen!(
+    Script(
+        name = "AddLiquidityScript",
+        abi = "scripts/add_liquidity_script/out/debug/add_liquidity_script-abi.json"
+    ),
+    Contract(
+        name = "MiraAMM",
+        abi = "fixtures/mira-amm/mira_amm_contract-abi.json"
+    ),
+    Contract(
+        name = "MockToken",
+        abi = "fixtures/mock-token/mock_token-abi.json"
+    )
+);
+
+pub mod amm {
+    use super::*;
+
+    pub async fn create_pool(
+        contract: &MiraAMM<WalletUnlocked>,
+        token_contract: &MockToken<WalletUnlocked>,
+        token_0_contract_id: ContractId,
+        token_0_sub_id: Bits256,
+        token_1_contract_id: ContractId,
+        token_1_sub_id: Bits256,
+        is_stable: bool,
+    ) -> FuelCallResponse<PoolId> {
+        contract
+            .methods()
+            .create_pool(
+                token_0_contract_id,
+                token_0_sub_id,
+                token_1_contract_id,
+                token_1_sub_id,
+                is_stable,
+            )
+            .with_contracts(&[token_contract])
+            .call()
+            .await
+            .unwrap()
+    }
+
+    pub async fn pool_metadata(
+        contract: &MiraAMM<WalletUnlocked>,
+        pool_id: PoolId,
+    ) -> FuelCallResponse<Option<PoolMetadata>> {
+        contract
+            .methods()
+            .pool_metadata(pool_id)
+            .call()
+            .await
+            .unwrap()
+    }
+}
+
+pub mod mock {
+    use super::*;
+
+    pub async fn deploy_mock_token_contract(
+        wallet: &WalletUnlocked,
+    ) -> (ContractId, MockToken<WalletUnlocked>) {
+        let contract_id = Contract::load_from(
+            MOCK_TOKEN_CONTRACT_BINARY_PATH,
+            LoadConfiguration::default(),
+        )
+        .unwrap()
+        .deploy(wallet, TxPolicies::default())
+        .await
+        .unwrap();
+
+        let id = ContractId::from(contract_id.clone());
+        let instance = MockToken::new(contract_id, wallet.clone());
+
+        (id, instance)
+    }
+
+    pub async fn add_token(
+        contract: &MockToken<WalletUnlocked>,
+        name: String,
+        symbol: String,
+        decimals: u8,
+    ) -> FuelCallResponse<AssetId> {
+        contract
+            .methods()
+            .add_token(name, symbol, decimals)
+            .call()
+            .await
+            .unwrap()
+    }
+
+    pub async fn mint_tokens(
+        contract: &MockToken<WalletUnlocked>,
+        asset_id: AssetId,
+        amount: u64,
+    ) -> FuelCallResponse<()> {
+        contract
+            .methods()
+            .mint_tokens(asset_id, amount)
+            .append_variable_outputs(1)
+            .call()
+            .await
+            .unwrap()
+    }
+
+    pub async fn get_sub_id(
+        contract: &MockToken<WalletUnlocked>,
+        asset_id: AssetId,
+    ) -> FuelCallResponse<Option<Bits256>> {
+        contract
+            .methods()
+            .get_sub_id(asset_id)
+            .call()
+            .await
+            .unwrap()
+    }
+}
+
+pub mod scripts {
+    use super::*;
+
+    pub const MAXIMUM_INPUT_AMOUNT: u64 = 100_000;
+
+    pub async fn get_transaction_inputs_outputs(
+        wallet: &WalletUnlocked,
+        assets: &Vec<(AssetId, u64)>,
+        variable_outputs: &Vec<(AssetId, Address)>,
+    ) -> (Vec<Input>, Vec<Output>) {
+        let mut inputs: Vec<Input> = vec![]; // capacity depends on wallet resources
+        let mut outputs: Vec<Output> = Vec::with_capacity(assets.len());
+
+        for (asset, amount) in assets {
+            let asset_inputs = wallet
+                .get_asset_inputs_for_amount(*asset, *amount)
+                .await
+                .unwrap();
+            inputs.extend(asset_inputs);
+            outputs.push(Output::Change {
+                asset_id: *asset,
+                amount: 0,
+                to: wallet.address().into(),
+            });
+        }
+
+        for (asset, to) in variable_outputs {
+            outputs.push(Output::Variable {
+                asset_id: *asset,
+                amount: 0,
+                to: *to,
+            });
+        }
+
+        (inputs, outputs)
+    }
+}
---
+++ test-harness/src/paths.rs
@@ -0,0 +1,5 @@
+pub const ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str =
+    "../../scripts/add_liquidity_script/out/debug/add_liquidity_script.bin";
+pub const AMM_CONTRACT_BINARY_PATH: &str = "../../fixtures/mira-amm/mira_amm_contract.bin";
+
+pub const MOCK_TOKEN_CONTRACT_BINARY_PATH: &str = "../../fixtures/mock-token/mock_token.bin";
